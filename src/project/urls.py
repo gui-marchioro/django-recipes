8dce9b2ea67448a5baf0c9b4bac4888551971082
Guilherme Marchioro <gui02_marchioro@hotmail.com>
2025-08-25 17:58:27 -0300

    Moved views from root project to recipes app

--- src/project/urls.py
+++ src/project/urls.py
@@ -16,14 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from django.http import HttpResponse, HttpRequest
-
-
-def my_view(request: HttpRequest) -> HttpResponse:
-    return HttpResponse("Sobre nós")
+from recipes.views import home, contato, sobre
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('sobre/', my_view),
+    path('', home),
+    path('contato/', contato),
+    path('sobre/', sobre),
 ]
--- src/recipes/views.py
+++ src/recipes/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse, HttpRequest
 
-# Create your views here.
+
+def home(request: HttpRequest) -> HttpResponse:
+    return HttpResponse("Home")
+
+
+def contato(request: HttpRequest) -> HttpResponse:
+    return HttpResponse("Contato")
+
+
+def sobre(request: HttpRequest) -> HttpResponse:
+    return HttpResponse("Sobre")
