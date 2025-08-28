3c5990620d8dde3c0f654ea41b874b92abdcc225
Guilherme Marchioro <gui02_marchioro@hotmail.com>
2025-08-28 13:27:34 -0300

    Added header partial page

--- src/recipes/views.py
+++ src/recipes/views.py
@@ -3,12 +3,4 @@ from django.http import HttpResponse, HttpRequest
 
 
 def home(request: HttpRequest) -> HttpResponse:
-    return render(request, "recipes/home.html")
-
-
-def contato(request: HttpRequest) -> HttpResponse:
-    return HttpResponse("Contato")
-
-
-def sobre(request: HttpRequest) -> HttpResponse:
-    return HttpResponse("Sobre")
+    return render(request, "recipes/pages/home.html")
