4f7d448a53d27a31107bb78404be80b7d249f52b
Guilherme Marchioro <gui02_marchioro@hotmail.com>
2025-09-02 14:27:26 -0300

    Added recipe detail view

--- src/recipes/views.py
+++ src/recipes/views.py
@@ -4,3 +4,7 @@ from django.http import HttpResponse, HttpRequest
 
 def home(request: HttpRequest) -> HttpResponse:
     return render(request, "recipes/pages/home.html")
+
+
+def recipe(request: HttpRequest, recipe_id: int) -> HttpResponse:
+    return render(request, "recipes/pages/recipe-view.html")
