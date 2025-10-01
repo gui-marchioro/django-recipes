5e174a715b44b324d7ea52e4620762d43d06b76c
Guilherme Marchioro <gui02_marchioro@hotmail.com>
2025-09-30 23:29:20 -0300

    Registered Category to admin panel

--- src/recipes/models.py
+++ src/recipes/models.py
@@ -4,9 +4,9 @@ from django.contrib.auth.models import User
 
 class Category(models.Model):
     name = models.CharField(max_length=65)
-    description = models.CharField(max_length=165)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
 
 
 class Recipe(models.Model):
