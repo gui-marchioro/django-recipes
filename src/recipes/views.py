da4c47f8b822be38909c940390b939a88a115b80
Guilherme Marchioro <gui02_marchioro@hotmail.com>
2025-10-04 15:50:43 -0300

    Added recipe filter by category in the home page

--- src/recipes/views.py
+++ src/recipes/views.py
@@ -1,11 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpRequest
 from utils.recipes.factory import make_recipe
+from .models import Recipe
 
 
 def home(request: HttpRequest) -> HttpResponse:
-    return render(request, "recipes/pages/home.html", context={
-        "recipes": [make_recipe() for _ in range(10)],
+    recipes = Recipe.objects.all().order_by('-id')
+    return render(request, 'recipes/pages/home.html', context={
+        'recipes': recipes,
+    })
+
+
+def category(request: HttpRequest, category_id: int) -> HttpResponse:
+    recipes = Recipe.objects.filter(
+        category__id=category_id
+    ).order_by('-id')
+    return render(request, 'recipes/pages/home.html', context={
+        'recipes': recipes,
     })
 
 
