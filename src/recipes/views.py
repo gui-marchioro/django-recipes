4a6d46af7c0446d5585ee8a65c3f1cb1ca238772
Guilherme Marchioro <gui02_marchioro@hotmail.com>
2025-10-15 09:37:40 -0300

    Added 404 error when accessing non-existing category page

--- src/recipes/views.py
+++ src/recipes/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse, HttpRequest
+from django.http import HttpResponse, HttpRequest, Http404
 from utils.recipes.factory import make_recipe
 from .models import Recipe
 
@@ -15,8 +15,15 @@ def category(request: HttpRequest, category_id: int) -> HttpResponse:
     recipes = Recipe.objects.filter(
         category__id=category_id, is_published=True
     ).order_by('-id')
+    recipe = recipes.first()
+    if not recipe:
+        raise Http404('Category not found')
+    category = recipe.category
+    if not category:
+        raise Http404('Category not found')
     return render(request, 'recipes/pages/category.html', context={
         'recipes': recipes,
+        'title': f'{category.name} - Category |',
     })
 
 
