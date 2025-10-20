a8c14f4c74d4c0233b801eb20259070b43bf4f27
Guilherme Marchioro <gui02_marchioro@hotmail.com>
2025-10-20 16:43:24 -0300

    Added search page using TDD

--- src/recipes/views.py
+++ src/recipes/views.py
@@ -1,3 +1,5 @@
+from django.db.models import Q
+from django.http.response import Http404
 from django.shortcuts import render, get_list_or_404, get_object_or_404
 from django.http import HttpResponse, HttpRequest, Http404
 from .models import Recipe
@@ -27,4 +29,25 @@ def recipe(request: HttpRequest, recipe_id: int) -> HttpResponse:
     return render(request, "recipes/pages/recipe-view.html", context={
         "recipe": recipe,
         "is_detail_page": True,
+    })
+
+
+def search(request: HttpRequest) -> HttpResponse:
+    search_term = request.GET.get('q', '').strip()
+
+    if not search_term:
+        raise Http404()
+
+    recipes = Recipe.objects.filter(
+        Q(
+            Q(title__icontains=search_term) |
+            Q(description__icontains=search_term),
+        ),
+        is_published=True
+    ).order_by('-id')
+
+    return render(request, 'recipes/pages/search.html', {
+        'page_title': f'Search for "{search_term}" |',
+        'search_term': search_term,
+        'recipes': recipes,
     })
