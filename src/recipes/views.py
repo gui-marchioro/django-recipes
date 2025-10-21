6e41356dde315b1bfe0ce609c9577a9d7742f4cc
Guilherme Marchioro <gui02_marchioro@hotmail.com>
2025-10-21 16:21:02 -0300

    Added pagination component to home page

--- src/recipes/views.py
+++ src/recipes/views.py
@@ -1,14 +1,31 @@
+from django.core.paginator import Paginator
 from django.db.models import Q
 from django.http.response import Http404
 from django.shortcuts import render, get_list_or_404, get_object_or_404
 from django.http import HttpResponse, HttpRequest, Http404
+from utils.pagination import make_pagination_range
 from .models import Recipe
 
 
 def home(request: HttpRequest) -> HttpResponse:
     recipes = Recipe.objects.all().filter(is_published=True).order_by('-id')
+
+    try:
+        current_page = int(request.GET.get('page', 1))
+    except ValueError:
+        current_page = 1
+    paginator = Paginator(recipes, 9)
+    page_obj = paginator.get_page(current_page)
+
+    pagination_range = make_pagination_range(
+        paginator.page_range,
+        4,
+        current_page
+    )
+
     return render(request, 'recipes/pages/home.html', context={
-        'recipes': recipes,
+        'recipes': page_obj,
+        'pagination_range': pagination_range
     })
 
 
