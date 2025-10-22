37cb48fe2d712afb745cdc94b648caa46aaf4795
Guilherme Marchioro <gui02_marchioro@hotmail.com>
2025-10-22 16:15:15 -0300

    redirecting author creation POST to another URL. Added CSS to author form

--- src/authors/views.py
+++ src/authors/views.py
@@ -1,12 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse, HttpRequest, Http404
 from .forms import RegisterForm
 
 
-def register_view(request):
-    if request.POST:
-        form = RegisterForm(request.POST)
-    else:
-        form = RegisterForm()
+def register_view(request: HttpRequest) -> HttpResponse:
+    register_form_data = request.session.get("register_form_data", None)
+    form = RegisterForm(register_form_data)
     return render(request, 'authors/pages/register.html', {
         'form': form,
     })
+
+
+def registered_view(request: HttpRequest) -> HttpResponse:
+    if not request.POST:
+        raise Http404()
+
+    request.session["register_form_data"] = request.POST
+    form = RegisterForm(request.POST)
+
+    return redirect("authors:register")
